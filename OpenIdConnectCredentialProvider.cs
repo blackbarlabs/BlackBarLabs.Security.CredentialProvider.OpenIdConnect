9539fea1b9589c76e3a594b0d088072d68e1631c
joshcodes <josh@blackbarlabs.com>
2016-03-08 19:24:14 -0600

    initia

---
+++ OpenIdConnectCredentialProvider.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace BlackBarLabs.Security.CredentialProvider.OpenIdConnect
+{
+    public class OpenIdConnectCredentialProvider : IProvideCredentials
+    {
+        public Task<string> RedeemTokenAsync(Uri providerId, string username, string accessToken)
+        {
+            var returnValue = String.Compare(username, accessToken) == 0 ? accessToken : default(string);
+            return Task.FromResult(returnValue);
+        }
+    }
+}
