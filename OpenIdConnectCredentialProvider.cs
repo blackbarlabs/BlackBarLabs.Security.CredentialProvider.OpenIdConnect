8e2e2b0116351c5dbf515684dd34aa405e7e67f2
Keith Holloway <keith@eastfive.com>
2016-04-08 11:26:01 -0500

    Added error messaging capabilities for credential checks

--- OpenIdConnectCredentialProvider.cs
+++ OpenIdConnectCredentialProvider.cs
@@ -6,10 +6,10 @@ namespace BlackBarLabs.Security.CredentialProvider.OpenIdConnect
     public class OpenIdConnectCredentialProvider : IProvideCredentials
     {
         public Task<TResult> RedeemTokenAsync<TResult>(Uri providerId, string username, string accessToken, 
-            Func<string, TResult> success, Func<TResult> invalidCredentials, Func<TResult> couldNotConnect)
+            Func<string, TResult> success, Func<string, TResult> invalidCredentials, Func<TResult> couldNotConnect)
         {
             var returnValue = String.Compare(username, accessToken) == 0 ? 
-                success(accessToken) : invalidCredentials();
+                success(accessToken) : invalidCredentials("Username and access token do not match");
             return Task.FromResult(returnValue);
         }
     }
